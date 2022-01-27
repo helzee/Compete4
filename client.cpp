bd20d54435f6a6e7c4cf406060f027d109b0fb53
helzee <joshhelzerman@gmail.com>
2022-01-26 16:50:12 -0800

    message echo. (commented out address connection

--- client.cpp
+++ client.cpp
@@ -3,7 +3,17 @@
  */
 
 #include "constants.h"
+#include <cstdio>
 #include <iostream>
+#include <arpa/inet.h> // inet_ntoa
+#include <netdb.h>       // gethostbyname
+#include <netinet/in.h>  // htonl, htons, inet_ntoa
+#include <netinet/tcp.h> // SO_REUSEADDR
+#include <pthread.h>
+#include <sys/socket.h> // socket, bind, listen, inet_ntoa
+#include <sys/types.h> // socket, bind
+#include <unistd.h>    // read, write, close
+#include <cstring>
 
 using namespace std;
 
@@ -30,5 +40,50 @@ int main(int argc, char **argv) {
    }
 
 
+   // create server info structure
+   struct addrinfo hints, *servInfo; // loaded with getaddrinfo()
+   memset(&hints, 0, sizeof(hints)); // initialize memory in struct hints
+   hints.ai_family = AF_UNSPEC;      // use IPv4 or IPv6
+   hints.ai_socktype = SOCK_STREAM;  // use TCP
 
+   // call getaddrinfo() to update servInfo
+   int error = getaddrinfo(serverName, serverPort, &hints, &servInfo);
+   if (error != 0) {
+      cerr << "getaddrinfo() Error! " << gai_strerror(error) << endl;
+      exit(1);
+   }
+
+   // make a socket
+   int clientSd = socket(servInfo->ai_family, servInfo->ai_socktype,
+                         servInfo->ai_protocol);
+   if (clientSd == -1) {
+      cerr << "Socket creation error!" << errno << endl;
+
+      return -1;
+   }
+   // lose pesky "Address already in use" error message
+   // int status = connect(clientSd, servInfo->ai_addr, servInfo->ai_addrlen);
+   // if (status < 0) {
+   //    cerr << "Failed to connect to the server" << errno << endl;
+
+   //    return -1;
+   // }
+
+   // This is the buffer that will store messages
+   // This stores messages to send and recieved messages.
+   char sendBuffer[MAX_MSG_SIZE];
+
+   
+
+   /** Initialization is completed. Socket is ready to use
+    * While loop is used until program termination.
+    * The while loop simply sends messages from the server, then
+    * receives and displays messages from the server.
+   */
+   while(1) {
+      if (fgets(sendBuffer, MAX_MSG_SIZE, stdin) != NULL) {
+         puts(sendBuffer);
+      }
+      }
 }
+
--- constants.h
+++ constants.h
@@ -4,3 +4,4 @@
 
 #define DEFAULT_SERVER "csslab7.uwb.edu"
 #define DEFAULT_PORT "13337"
+#define MAX_MSG_SIZE 128

a3c54be23486c540f4453cf294bc8a605ed476e8
Alex Lambert <hyperactive@live.com>
2022-02-15 10:14:19 -0800

    Added structure for enabling logging in / making accounts

--- commandParser.cpp
+++ commandParser.cpp
@@ -16,6 +16,12 @@ bool parseCommand(string command, Session* session)
       mainMenuCommand(command, session);
    case LOGIN:
       loginMenuCommand(command, session);
+   case SIGNIN:
+      signInCommand(command, session);
+   case MAKEACCOUNT:
+      makeAccountCommand(command, session);
+   case GUEST:
+      signInAsGuestCommand(command, session);
    default:
       send("ERROR: You are went to an invalid menu, sending back to main menu",
            session->clientSd);
@@ -55,15 +61,63 @@ bool loginMenuCommand(string command, Session* session)
       return;
    }
    if (cmp(command, "s")) {
-      // Sign in page
+      session->currMenu = SIGNIN;
+      signInCommand("print", session);
       return;
    }
    if (cmp(command, "m")) {
-      // Make account page
+      session->currMenu = MAKEACCOUNT;
+      makeAccountCommand("print", session);
       return;
    }
    if (cmp(command, "g")) {
-      // Guest login page
+      session->currMenu = GUEST;
+      signInAsGuestCommand("print", session);
+      return;
+   }
+
+   send("Not a recognized command, try again.\n", session->clientSd);
+}
+
+void signInCommand(string command, Session* session)
+{
+   if (cmp(command, "main") || cmp(command, "back")) {
+      session->currMenu = MAIN;
+      mainMenuCommand("print", session);
+      return;
+   }
+   if (cmp(command, "print")) {
+      send("Please enter username to sign in: ", session->clientSd);
+      return;
+   }
+
+   send("Not a recognized command, try again.\n", session->clientSd);
+}
+
+void makeAccountCommand(string command, Session* session)
+{
+   if (cmp(command, "main") || cmp(command, "back")) {
+      session->currMenu = MAIN;
+      mainMenuCommand("print", session);
+      return;
+   }
+   if (cmp(command, "print")) {
+      send("Please enter your desired username: ", session->clientSd);
+      return;
+   }
+
+   send("Not a recognized command, try again.\n", session->clientSd);
+}
+
+void signInAsGuestCommand(string command, Session* session)
+{
+   if (cmp(command, "main") || cmp(command, "back")) {
+      session->currMenu = MAIN;
+      mainMenuCommand("print", session);
+      return;
+   }
+   if (cmp(command, "print")) {
+      send("Please enter your desired guest name: ", session->clientSd);
       return;
    }
 
--- commandParser.h
+++ commandParser.h
@@ -1,14 +1,18 @@
 #include "constants.h"
 #include "globalFuncs.h"
 #include "session.h"
+#include "userRecordDB.h"
 
 #ifndef COMMANDPARSER_H
 #define COMMANDPARSER_H
 
 using namespace std;
 
-bool parseCommand(string command, Session*);
+bool parseCommand(string, Session*);
 void mainMenuCommand(string, Session*);
 void loginMenuCommand(string, Session*);
+void signInCommand(string, Session*);
+void makeAccountCommand(string, Session*);
+void signInAsGuestCommand(string, Session*);
 
 #endif

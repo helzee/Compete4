2fe981b01e019361824e30ad96cb472fd15dbf61
Alex Lambert <hyperactive@live.com>
2022-02-14 16:31:09 -0800

    Fixed the sessionsDB to be mostly complete

--- sessionsDB.cpp
+++ sessionsDB.cpp
@@ -4,11 +4,21 @@
 
 using namespace std;
 
-unordered_map<int, Session> sessionMap = new unordered_map<int Session>();
+auto sessionMap = new unordered_map<int, Session*>();
 
 Session* makeSession()
 {
    Session* newSession = new Session();
    sessionMap.insert(newSession->getSessionID(), newSession);
    return newSession;
+}
+
+bool removeSession(int sessionID) { sessionMap.erase(sessionID) == 1; }
+
+Session* getSession(int sessionID)
+{
+   auto session = sessionMap.find(sessionID);
+   if (session == sessionMap.end())
+      return nullptr;
+   return &session;
 }

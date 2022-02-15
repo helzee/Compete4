c54e7bcc357e03d575724b564cb7b0819d02b558
Alex Lambert <hyperactive@live.com>
2022-02-14 16:10:18 -0800

    Added session list

--- sessionsDB.cpp
+++ sessionsDB.cpp
@@ -1,8 +1,14 @@
 #include "constants.h"
 #include "session.cpp"
+#include <vector>
+
+using namespace std;
+
+vector<Session> sessions = new vector<Session>;
 
 Session* makeSession()
 {
    Session* newSession = new Session();
+   sessions.push_back(newSession);
    return newSession;
 }

3ba432c16b0adbe266ca2df4f3c8f00cf487874d
helzee <joshhelzerman@gmail.com>
2022-02-24 07:55:43 -0800

    removed newline redundancy

--- commandParser.cpp
+++ commandParser.cpp
@@ -9,10 +9,9 @@
 // Return false if the command was quit/exit
 int parseCommand(string command, Session* session)
 {
-   // lexer needs newline to work
-   string newLineComm = command + "\n";
+   
    // first step: check for global commands
-   CommandTok* commTok = commandLexer.lexCommand(newLineComm.c_str());
+   CommandTok* commTok = commandLexer.lexCommand(command.c_str());
    return session->handleCommand(commTok);
    
 }

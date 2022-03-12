3f473d3d61d414c0d89ba78da8453e2a4ab17c64
Joseph Collora <j.collora@outlook.com>
2022-03-11 18:28:30 -0800

    quick comment add

--- globalFuncs.cpp
+++ globalFuncs.cpp
@@ -1,11 +1,13 @@
 #include "globalFuncs.h"
 
+// sends param message via socket descriptor param
 void send(string message, int sd)
 {
    while (write(sd, message.c_str(), MAX_MSG_SIZE) != MAX_MSG_SIZE)
       ;
 }
 
+// returns recieved string on socked descriptor passed via param
 string receive(int sd)
 {
    char rcvBuffer[MAX_MSG_SIZE];
@@ -25,4 +27,5 @@ string receive(int sd)
    return command;
 }
 
+// simple string comparison method, returns true if param a and b are equal
 bool cmp(string a, string b) { return a.compare(b) == 0; }
